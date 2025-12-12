95cec5d3c8e0eecc303088d67385a86ea3b3511a
larbi-ait <larbi.aitabdelmalek@hotmail.com>
2025-12-12 19:24:27 +0100

    Implement piece placement in chess game
    Added piece placement logic in the Game class.

--- Chess.py
+++ Chess.py
@@ -2,6 +2,7 @@ class Game:
     
     def __init__(self):
         self.plateau = self.creer_plateau()
+        self.placement()
     
     def creer_plateau(self):
         grille = []
@@ -15,7 +16,26 @@ class Game:
             print(self.plateau[i])
     
     def placement(self):
-        pass
+        vide = 0
+        pion = 1
+        cavalier = 2
+        fou = 3
+        tour = 4
+        dame = 5
+        roi = 6
+        self.plateau[0] = [tour, cavalier, fou, dame, roi, fou, cavalier, tour]
+        self.plateau[1] = [pion] * 8
+        self.plateau[6] = [pion] * 8
+        self.plateau[7] = [tour, cavalier, fou, dame, roi, fou, cavalier, tour]
+    
+    
+    
+    
+class Noir:
+    pass
+
+class Blanc:
+    pass
         
     
 chess = Game()
